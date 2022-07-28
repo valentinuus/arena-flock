e6e30fefe374db689bd655dab4e2fd59d5d1153d
valentinuus <72602932+valentinuus@users.noreply.github.com>
2022-07-28 13:43:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //================test WEBP=============
-// function testWebP(callback) {
-// 	var webP = new Image();
-// 	webP.onload = webP.onerror = function () {
-// 		callback(webP.height == 2);
-// 	};
-// 	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-// }
-// testWebP(function (support) {
-// 	if (support === true) {
-// 		document.querySelector('html').classList.add('_webp');
-// 	} else {
-// 		document.querySelector('html').classList.add('_no-webp');
-// 	}
-// });
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support === true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+});
 //================test WEBP=============
 
 
