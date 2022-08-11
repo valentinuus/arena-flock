71859bbfa58edd0af203d19ca4b723e8e7e0e97b
valentinuus <72602932+valentinuus@users.noreply.github.com>
2022-08-11 18:49:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,161 @@
+//================test WEBP=============
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support === true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+});
+//================test WEBP=============
+
+
+
+//==================ibg=================
+//==========скрипт подставляющий картинку вместо фона=================
+// function ibg() {
+// 	let ibg = document.querySelectorAll(".ibg");
+// 	for (var i = 0; i < ibg.length; i++) {
+// 		if (ibg[i].querySelector('img')) {
+// 			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+// 		}
+// 	}
+// }
+// ibg();
+//==================ibg=================
+
+
+//==================toggleClass('active'),toggleClass('lock')=================
+// ==========добавление классов для правильной работы бургера=================
+// $(document).ready(function () {
+// 	$('.menu__body').click(function (event) {
+// 		$('.menu__header,.menu__burger,.menu__logo').toggleClass('active');
+// 		$('body').toggleClass('lock');
+// 	});
+// });
+//==================toggleClass('active'),toggleClass('lock')=================
+
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	}
+} else {
+	document.body.classList.add('_pc');
+}
+
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+// Меню бургер
+
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+};
+// Прокрутка при клике
+
+
+// Слайд
+
+$('.slider-for').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	fade: true,
+	asNavFor: '.slider-nav',
+	dots: false,
+});
+
+
+
+$('.slider-nav').slick({
+	slidesToShow: 6,
+	slidesToScroll: 1,
+	asNavFor: '.slider-for',
+	dots: false,
+	centerMode: true,
+	arrows: false,
+	focusOnSelect: true,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2
+			}
+		}
+	]
+});
